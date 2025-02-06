73e5ec8e27f0d752685f574aa48e56550b7dcadb
iamnikhilranjan <nikhil21907@gamil.com>
2025-02-06 18:24:32 +0530

    Updated event images

--- config/glimpse.js
+++ config/glimpse.js
@@ -1,19 +1,19 @@
 export const glimpseConfig = [
   {
     id: "1",
-    content: "4000 Hackers",
-    content2: "40 Partners",
-    src1: "/images/glimpse/glimpse_hackatron.jpg",
-    src2: "/images/glimpse/glimpse_hackatron.jpg",
-    src3: "/images/glimpse/glimpse_hackatron.jpg",
+    content: "500 Hackers",
+    content2: "10 Partners",
+    src1: "/images/glimpse/glimpse-3.jpg",
+    src2: "/images/glimpse/glimpse-11.png",
+    src3: "/images/glimpse/glimpse-1.jpg",
   },
   {
     id: "2",
-    content: "450 Projects",
+    content: "100 Projects",
     content2: "36 Hack Hours",
-    src1: "/images/glimpse/glimpse_hackatron.jpg",
-    src2: "/images/glimpse/glimpse_hackatron.jpg",
-    src3: "/images/glimpse/glimpse_hackatron.jpg",
+    src1: "/images/glimpse/glimpse-2.png",
+    src2: "/images/glimpse/glimpse-12.jpeg",
+    src3: "/images/glimpse/glimpse-10.jpg",
     className: "row-reverse",
   },
 ];
